6678216edc7212e3d16402d47c8dfb54b65ed6af
Aaron Mikulka <aaronjmikulka@gmail.com>
2022-11-07 09:59:17 -0700

    Working grocery list

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // TODO
+const { useState } = React;
+let GroceryList = ({items}) => {
+  return (
+    <ul>
+      {items.map((GroceryItem) => (
+        <GroceryListItem item={GroceryItem} />
+      ))}
+    </ul>
+  )
+};
+
+function GroceryListItem ({item}) {
+  const [isDone, setIsDone] = useState(false);
+  const style = {
+    fontWeight: isDone ? 'bold' : 'normal'
+  };
+  function handleClick() {
+   setIsDone(!isDone);
+  }
+
+  return (
+    <li style={style} onClick={handleClick}>{item}</li>
+  )
+}
+let items = ['Apples', 'Bananas'];
+
+ReactDOM.render(<GroceryList items={items} />, document.getElementById('app'));
